36da6cbce406ee5a8adca6daf87cdba4022071b0
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-09-16 08:17:18 +0200

    feat: finished the basic design

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,8 +1,8 @@
 document.getElementById("dropDownButton").addEventListener("click", function () {
   let dropDownMenu = document.getElementById("dropdown-menu");
-  if (dropDownMenu.style.display === "none") {
-    dropDownMenu.style.display = "block";
-  } else {
+  if (dropDownMenu.style.display === "block") {
     dropDownMenu.style.display = "none";
+  } else {
+    dropDownMenu.style.display = "block";
   }
 })
--- js/main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("dropDownButton").addEventListener("click", function () {
-  document.getElementById("dropdown-menu").classList.toggle("hide");
-})
