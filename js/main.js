6488b18bd9c1972ecdd31d9684543bd083d4dff8
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-09-19 09:19:38 +0200

    refactor: extracted selectPieces method fix: user can no longer select a removed piece

--- js/main.js
+++ js/main.js
@@ -16,16 +16,7 @@ function shuffleArray(array) {
 
 
 function createGame() {
-  let layout = [
-    [0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+  let layout = [[0, 0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
   let pieces = ['MJd1-.svg.png', 'MJd2-.svg.png', 'MJd3-.svg.png', 'MJf1-.svg.png', 'MJf2-.svg.png']
 
@@ -45,22 +36,7 @@ function createGame() {
 
         piece.innerHTML = '<img src="img/Pieces/' + pieces[Math.floor(curPiece)] + '" alt="Mahjong piece">'
         curPiece += 0.5;
-
-        //TO-DO check if the piece was already deleted
-        piece.addEventListener('click', () => {
-          if (selected.length === 0) {
-            selected.push(piece)
-            selected[0].className = 'selected'
-          } else {
-            if (selected[0].innerHTML === piece.innerHTML && selected[0] !== piece) {
-              selected[0].className = 'empty-place'
-              selected[0].innerHTML = ''
-              piece.className = 'empty-place'
-              piece.innerHTML = ''
-              selected = []
-            }
-          }
-        })
+        piece.addEventListener('click', () => selectPieces(piece))
       } else {
         piece.className = 'empty-place'
       }
@@ -71,6 +47,24 @@ function createGame() {
       document.getElementById('game').appendChild(piece);
     });
   });
+}
+
+function selectPieces(piece) {
+
+  if (piece.className === 'empty-place') {
+
+  } else if (selected.length === 0) {  //  Highlight selected piece
+    selected.push(piece)
+    selected[0].className = 'selected'
+  } else if (selected[0].innerHTML === piece.innerHTML && selected[0] !== piece) { // Remove selected pieces if they're of the same type
+    selected[0].className = 'empty-place'
+    piece.className = 'empty-place'
+
+    selected[0].innerHTML = ''
+    piece.innerHTML = ''
+
+    selected = []
+  }
 }
 
 function newGame() {
