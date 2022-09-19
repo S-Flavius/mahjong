ce764342606a0e50bc1800a53d0bad06a05a843b
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-09-19 09:03:45 +0200

    refactor: changed CSS class names feat: added pictures to the pieces feat: added check if the pieces are of the same kind

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-createGame()
+window.onload = () => createGame()
+
 
 document.getElementById('new-game').addEventListener('click', newGame)
 
 let selected = []
+
+
+// https://stackoverflow.com/a/12646864
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 
 function createGame() {
   let layout = [
@@ -14,34 +25,44 @@ function createGame() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ];
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
+  let pieces = ['MJd1-.svg.png', 'MJd2-.svg.png', 'MJd3-.svg.png', 'MJf1-.svg.png', 'MJf2-.svg.png']
 
   let pieceWidth = document.getElementById('game').offsetWidth / layout[0].length;
   let pieceHeight = document.getElementById('game').offsetHeight / layout.length;
+
+  shuffleArray(pieces);
+
+  let curPiece = 0;
 
   layout.forEach((row, rowIndex) => {
     row.forEach((square, squareIndex) => {
       let piece = document.createElement('div');
       if (square === 1) {
-        piece.className = 'black-piece'
-        piece.addEventListener('click', () => {
+        console.log();
+        piece.className = 'taken-place'
 
+        piece.innerHTML = '<img src="img/Pieces/' + pieces[Math.floor(curPiece)] + '" alt="Mahjong piece">'
+        curPiece += 0.5;
+
+        //TO-DO check if the piece was already deleted
+        piece.addEventListener('click', () => {
           if (selected.length === 0) {
             selected.push(piece)
+            selected[0].className = 'selected'
           } else {
-            if (selected[0].id === piece.id && selected[0] !== piece) {
-              selected[0].className = 'white-piece'
-              piece.className = 'white-piece'
+            if (selected[0].innerHTML === piece.innerHTML && selected[0] !== piece) {
+              selected[0].className = 'empty-place'
+              selected[0].innerHTML = ''
+              piece.className = 'empty-place'
+              piece.innerHTML = ''
               selected = []
             }
-
           }
-
         })
       } else {
-        piece.className = 'white-piece'
+        piece.className = 'empty-place'
       }
       piece.style.width = `${pieceWidth}px`;
       piece.style.height = `${pieceHeight}px`;
