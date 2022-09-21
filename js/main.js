0e4ebb483fa9d75768ffd11db009eb533727d689
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-09-21 16:08:17 +0200

    fix: "New game" now works again

--- js/main.js
+++ js/main.js
@@ -8,12 +8,16 @@ let selected = []
 
 
 let layouts = [// flower
-  [[0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0, 1, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 0, 1, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0, 0]], // pyramid
-  [[0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 2, 1, 1, 0, 0], [0, 0, 1, 2, 3, 2, 1, 0, 0], [1, 1, 2, 3, 4, 3, 2, 1, 1], [0, 0, 1, 2, 3, 2, 1, 0, 0], [0, 0, 1, 1, 2, 1, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0]], // snake
+  [[0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0, 1, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 0, 1, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0, 0]],
+
+  // pyramid
+  [[0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 2, 1, 1, 0, 0], [0, 0, 1, 2, 3, 2, 1, 0, 0], [1, 1, 2, 3, 4, 3, 2, 1, 1], [0, 0, 1, 2, 3, 2, 1, 0, 0], [0, 0, 1, 1, 2, 1, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0]],
+
+  // snake
   [[1, 2, 0, 1, 1, 2, 1, 1, 0], [1, 2, 0, 1, 2, 3, 2, 1, 0], [1, 3, 0, 1, 1, 4, 4, 1, 0], [1, 2, 0, 0, 0, 0, 3, 0, 0], [3, 2, 0, 0, 0, 1, 2, 3, 0], [0, 2, 0, 0, 0, 2, 0, 0, 0], [0, 2, 3, 0, 4, 3, 0, 0, 0], [0, 0, 3, 0, 4, 0, 0, 0, 0], [0, 0, 3, 3, 4, 0, 0, 0, 0]]];
 
-let layoutNumber = Math.floor(Math.random() * layouts.length)
-let chosenLayout = layouts[layoutNumber].map(x => x)
+let layoutNumber = 0;
+let chosenLayout = 0;
 
 document.getElementById('flower').addEventListener('click', () => {
   changeLayout(0)
@@ -28,7 +32,6 @@ document.getElementById('snake').addEventListener('click', () => {
 
 function changeLayout(layoutNr) {
   layoutNumber = layoutNr
-  chosenLayout = layouts[layoutNumber].map(x => x)
   newGame()
 }
 
@@ -41,7 +44,9 @@ function shuffleArray(array) {
 }
 
 function createGame() {
-
+  chosenLayout = JSON.parse(JSON.stringify(layouts[layoutNumber].slice(0)))
+  // This is call by reference for whatever reason........................
+  // chosenLayout = layouts[layoutNumber].slice(0)
   let pieces = ['MJd1-.svg.png', 'MJd2-.svg.png', 'MJd3-.svg.png', 'MJf1-.svg.png', 'MJf2-.svg.png', 'MJf3-.svg.png', 'MJf4-.svg.png', 'MJh1-.svg.png', 'MJh2-.svg.png', 'MJh3-.svg.png', 'MJh4-.svg.png', 'MJh5-.svg.png', 'MJh6-.svg.png', 'MJh7-.svg.png', 'MJh8-.svg.png', 'MJs1-.svg.png', 'MJs2-.svg.png', 'MJs3-.svg.png', 'MJs4-.svg.png', 'MJs5-.svg.png', 'MJs6-.svg.png', 'MJs7-.svg.png', 'MJs8-.svg.png', 'MJs9-.svg.png', 'MJt1-.svg.png', 'MJt2-.svg.png', 'MJt3-.svg.png', 'MJt4-.svg.png', 'MJt5-.svg.png', 'MJt6-.svg.png', 'MJt7-.svg.png', 'MJt8-.svg.png', 'MJt9-.svg.png', 'MJw1-.svg.png', 'MJw2-.svg.png', 'MJw3-.svg.png', 'MJw4-.svg.png', 'MJw5-.svg.png', 'MJw6-.svg.png', 'MJw7-.svg.png', 'MJw8-.svg.png', 'MJw9-.svg.png']
 
   document.getElementById('dropdown-menu').children[0].children[layoutNumber].className += ' is-active'
@@ -60,8 +65,8 @@ function createGame() {
     hasPieces = false
     for (let rowIndex = 0; rowIndex < chosenLayout.length; rowIndex++) {
       for (let columnIndex = 0; columnIndex < chosenLayout[rowIndex].length; columnIndex++) {
-        let piece = document.createElement('div');
         if (chosenLayout[rowIndex][columnIndex] !== 0) {
+          let piece = document.createElement('div');
           hasPieces = true
           piece.className = 'piece'
 
@@ -69,14 +74,14 @@ function createGame() {
           curPiece += 0.5;
           piece.addEventListener('click', () => selectPieces(piece))
           chosenLayout[rowIndex][columnIndex]--;
+          piece.style.width = `${pieceWidth}px`;
+          piece.style.height = `${pieceHeight}px`;
+          piece.style.left = `${rowIndex * pieceWidth}px`;
+          // piece.style.left = `${rowIndex * pieceWidth - square*5}px`;
+          piece.style.top = `${columnIndex * pieceHeight}px`;
+          // piece.style.top = `${columnIndex * pieceHeight + square * 10}px`;
+          grid.children[rowIndex].children[columnIndex].appendChild(piece);
         }
-        piece.style.width = `${pieceWidth}px`;
-        piece.style.height = `${pieceHeight}px`;
-        piece.style.left = `${rowIndex * pieceWidth}px`;
-        // piece.style.left = `${rowIndex * pieceWidth - square*5}px`;
-        piece.style.top = `${columnIndex * pieceHeight}px`;
-        // piece.style.top = `${columnIndex * pieceHeight + square * 10}px`;
-        grid.children[rowIndex].children[columnIndex].appendChild(piece);
       }
     }
   }
