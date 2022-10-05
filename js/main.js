55173d3b47fb4c72bf2feccc8eba81b565c202a5
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-10-05 09:31:08 +0200

    feat: started to abstract the layouts

--- js/main.js
+++ js/main.js
@@ -145,7 +145,7 @@ function generatePiece(colIndex, rowIndex, pieces, curPiece, pieceWidth, pieceHe
   piece.style.width = `${pieceWidth}px`;
   piece.style.height = `${pieceHeight}px`;
   piece.style.left = `${colIndex * (0.77 * pieceWidth) - (piecesOnPosition - 1) * 9 + (chosenLayout.length / 10 * pieceWidth)}px`;
-  piece.style.top = `${rowIndex * 0.84 * pieceHeight + (piecesOnPosition - 1) * 7}px`;
+  piece.style.top = `${rowIndex * 0.84 * pieceHeight + (piecesOnPosition - 1) * 7 + 2}px`;
   document.getElementById('game').children[colIndex].children[rowIndex].appendChild(piece);
 }
 
