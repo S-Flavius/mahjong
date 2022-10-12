5b3ba41bc6ad060bbad3c2e23c0ef19e1181b6e2
fsecheli <Flavius.Secheli@edvschule-plattling.de>
2022-10-12 08:23:37 +0200

    chore: dropdown menus now hide on any click on the page

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,16 +1,41 @@
-document.getElementById('dropDownButton').addEventListener('click', function () {
+// noinspection DuplicatedCode
+
+let wasMenu1Clicked = false;
+let wasMenu2Clicked = false;
+
+window.addEventListener('click', () => {
+
+  if (wasMenu1Clicked) {
+    wasMenu1Clicked = false;
+    document.getElementById('dropdown-menu2').style.display = 'none';
+    return;
+  } else if (wasMenu2Clicked) {
+    wasMenu2Clicked = false;
+    document.getElementById('dropdown-menu').style.display = 'none';
+    return;
+  }
+
+  document.getElementById('dropdown-menu').style.display = 'none';
+  document.getElementById('dropdown-menu2').style.display = 'none';
+});
+
+document.getElementById('dropDownButton').addEventListener('click', () => {
   let dropDownMenu = document.getElementById('dropdown-menu');
   if (dropDownMenu.style.display === 'block') {
     dropDownMenu.style.display = 'none';
   } else {
     dropDownMenu.style.display = 'block';
   }
+  wasMenu1Clicked = true;
 });
-document.getElementById('dropDownButton2').addEventListener('click', function () {
+
+document.getElementById('dropDownButton2').addEventListener('click', () => {
   let dropDownMenu = document.getElementById('dropdown-menu2');
   if (dropDownMenu.style.display === 'block') {
     dropDownMenu.style.display = 'none';
   } else {
     dropDownMenu.style.display = 'block';
   }
+
+  wasMenu2Clicked = true;
 });
